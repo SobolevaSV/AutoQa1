5657bb15f209d48047f38da53e94a1d60f4e4d07
Svetlana Soboleva <blankor@mail.ru>
2023-12-10 00:19:59 +0400

    Testng commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,7 +6,7 @@ import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
-    public void Remain1() { // если покупаем на 999 руб., то сообщает докупить еще на 1 руб.
+    public void Remain1() { // при покупке на 999 руб. сервис сообщает докупить еще на 1 руб.
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int actual = service.remain(amount);
@@ -15,7 +15,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void Remain2() { // если покупаем на 1000 руб., то сообщает докупить еще на 0 руб.
+    public void Remain2() { // при покупке на 1000 руб. сервис сообщает докупить еще на 0 руб.
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
@@ -24,16 +24,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void Remain3() { // если покупаем на 1000 руб., то не должен сообщать докупить еще на 1000 руб.
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int actual = service.remain(amount);
-        int expected = 1000;
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void Remain4() { // если покупаем на 1001 руб., то сообщает докупить еще на 999 руб.
+    public void Remain3() { // при покупке на 1001 руб. сервис сообщает докупить еще на 999 руб.
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int actual = service.remain(amount);
